411c5de529bf9bd93ec23b6e2d83549e45738d55
Leonardo <l.losapio1@studenti.unisa.it>
2023-11-21 17:38:49 +0100

    testing

--- TaskAutomation1/src/taskautomation1/TaskAutomation.java
+++ TaskAutomation1/src/taskautomation1/TaskAutomation.java
@@ -21,7 +21,7 @@ public class TaskAutomation extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        //test test
+        //test test 2222
         stage.setScene(scene);
         stage.show();
     }
