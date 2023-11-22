de5e9143d254ea0441bdd82ae7a0573fc482e24a
Leonardo <l.losapio1@studenti.unisa.it>
2023-11-22 10:28:55 +0100

    removed 'hello world' button and label from fxml controller

--- TaskAutomation/src/taskautomation/FXMLDocumentController.java
+++ TaskAutomation/src/taskautomation/FXMLDocumentController.java
@@ -9,7 +9,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 /**
  *
@@ -17,18 +19,19 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
+    private Label title;
     @FXML
-    private Label label;
-    
+    private ListView<?> ruleListView;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Button addRuleButton;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void addRuleButtonAction(ActionEvent event) {
+    }
     
 }
