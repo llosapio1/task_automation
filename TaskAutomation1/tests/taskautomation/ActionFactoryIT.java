f42f296fe16c5e5189a4dd6c4fb1352e7e38c5e1
Alejandro <Alejandro@DESKTOP-IJ0RFSQ.mshome.net>
2023-11-25 03:31:00 +0100

    ActionFactory test

---
+++ TaskAutomation1/tests/taskautomation/ActionFactoryIT.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import taskautomation.ActionFactory;
+import taskautomation.azioni.Action;
+import taskautomation.azioni.BasicAction;
+import taskautomation.azioni.DisplayMessageAction;
+
+
+/**
+ *
+ * @author alessandro
+ */
+public class ActionFactoryIT{
+    
+    private ActionFactory instance;
+    
+    public ActionFactoryIT() {
+
+    }
+    
+    /**
+     * Test of create method, of class TriggerFactory.
+     */
+    @Test
+    public void testCreateWithValidType() {  
+        String ActionType = "DisplayMessageAction";
+        new ActionFactory().create(ActionType);
+        Action result = new ActionFactory().create(ActionType);
+        assertTrue(result instanceof DisplayMessageAction);
+        
+    }
+    
+    @Test
+    public void testCreateWithInvalidType() {
+        String ActionType = "InvalidType";
+        Action result = new ActionFactory().create(ActionType);
+        assertTrue(result == null);
+        
+    }
+    
+    @Test
+    public void testCreateWithNullType() {
+        Action result = new ActionFactory().create(null);
+        assertTrue(result == null);
+        
+    }
+    
+    
+    
+}
