4cfa5b07cceb864339081c8dc92a4da7209aa2cc
Leonardo <l.losapio1@studenti.unisa.it>
2023-11-30 09:19:04 +0100

    added AppendStringToFile, CopyFileToDir, DeleteFile, MoveFileBetweenDirs decorators

---
+++ TaskAutomation1/src/taskautomation/azioni/AppendStringToFileDecorator.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taskautomation.azioni;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Leonardo
+ */
+public class AppendStringToFileDecorator extends ActionDecorator{
+    private File file;
+    private String string;
+   
+
+    public AppendStringToFileDecorator(File file, String string, Action BasicAction) {
+        super(BasicAction);
+        this.file = file;
+        this.string = string;
+        
+    }
+
+    /**
+     *
+     * @throws IOException
+     */
+    @Override
+    public void executeAction(){
+       
+        try (FileWriter fr = new FileWriter(file.getAbsolutePath(), true)) {
+            fr.write(string);
+            fr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(AppendStringToFileDecorator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
