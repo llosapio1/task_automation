95103408d50ece424e46717de3c487e9e98894b9
Leonardo <l.losapio1@studenti.unisa.it>
2023-12-02 12:56:51 +0100

    moved responsibilities from action factory to action classes

--- TaskAutomation1/src/taskautomation/azioni/DeleteFileDecorator.java
+++ TaskAutomation1/src/taskautomation/azioni/DeleteFileDecorator.java
@@ -5,20 +5,36 @@
 package taskautomation.azioni;
 
 import java.io.File;
+import java.io.Serializable;
+import javax.swing.JFileChooser;
 
 /**
  *
  * @author Leonardo
  */
-public class DeleteFileDecorator extends ActionDecorator{
+public class DeleteFileDecorator extends ActionDecorator implements Serializable{
     File selectedFile;
-    public DeleteFileDecorator(File selectedFile, Action BasicAction) {
-        super(BasicAction);
+    
+    public DeleteFileDecorator(File selectedFile, Action basicAction) {
+        super(basicAction);
         this.selectedFile = selectedFile;
+    }
+    public DeleteFileDecorator(Action BasicAction){
+        super(BasicAction);
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("select file to delete");
+           
+        int res = fileChooser.showOpenDialog(null);
+           if(res == JFileChooser.APPROVE_OPTION){
+               this.selectedFile = fileChooser.getSelectedFile();
+               
+           }
+    
     }
     
     @Override
     public void executeAction(){
+       
         selectedFile.delete();
     }
      @Override
