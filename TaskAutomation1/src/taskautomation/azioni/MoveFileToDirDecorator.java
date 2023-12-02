6317a53a976bff56c91110c32869af699a8bb17a
Leonardo <l.losapio1@studenti.unisa.it>
2023-12-02 12:56:57 +0100

    moved responsibilities from action factory to action classes

--- TaskAutomation1/src/taskautomation/azioni/MoveFileToDirDecorator.java
+++ TaskAutomation1/src/taskautomation/azioni/MoveFileToDirDecorator.java
@@ -12,6 +12,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFileChooser;
 
 /**
  *
@@ -23,16 +24,35 @@ public class MoveFileToDirDecorator extends ActionDecorator implements Serializa
     String destDirPath;
     File destDir;
     
-    public MoveFileToDirDecorator(File selectedFile, File destinationDir, Action BasicAction) {
-        super(BasicAction);
+    public MoveFileToDirDecorator(File selectedFile, File destinationDir, Action basicAction) {
+        super(basicAction);
        
         this.selectedFile = selectedFile;
         this.destDir = destinationDir;
         destDirPath = destinationDir.getAbsolutePath();
     }
-    
+    public MoveFileToDirDecorator(Action basicAction){
+        super(basicAction);
+        JFileChooser fileChooser1 = new JFileChooser();
+            fileChooser1.setDialogTitle("choose file to move");
+            int res1 = fileChooser1.showOpenDialog(null);
+            
+            
+            JFileChooser fileChooser2 = new JFileChooser();
+            fileChooser2.setDialogTitle("choose destination folder");
+            fileChooser2.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int res2= fileChooser2.showOpenDialog(null);
+            
+           if(res1 == JFileChooser.APPROVE_OPTION && res2 == JFileChooser.APPROVE_OPTION){
+               this.selectedFile = fileChooser1.getSelectedFile();  
+               this.destDir = fileChooser2.getSelectedFile();
+               this.destDirPath = this.destDir.getAbsolutePath();
+               
+           } 
+    }
     @Override
     public void executeAction(){
+        System.out.println("inside execute move with file " + selectedFile.toString() + " and dir " + destDir.toString());
         if(selectedFile.exists()){
        try {
            Files.move(selectedFile.toPath(),
