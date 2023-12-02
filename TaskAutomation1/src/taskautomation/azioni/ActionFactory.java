d5074d0023f5c3e7d2a28017763561b57a848f1d
Leonardo <l.losapio1@studenti.unisa.it>
2023-12-02 12:56:31 +0100

    moved responsibilities from action factory to action classes

--- TaskAutomation1/src/taskautomation/azioni/ActionFactory.java
+++ TaskAutomation1/src/taskautomation/azioni/ActionFactory.java
@@ -37,65 +37,20 @@ public class ActionFactory{
                 return null;
             }
         } else if(selectedType.equalsIgnoreCase("AppendStringToFile")){
-            String string = JOptionPane.showInputDialog("Type the string to append.");
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setDialogTitle("select file to append string to");
-           FileNameExtensionFilter filter = new FileNameExtensionFilter("text files(*.txt)", "txt");
-           fileChooser.setFileFilter(filter);
-            int res = fileChooser.showOpenDialog(null);
-           if(res == JFileChooser.APPROVE_OPTION){
-               File selectedFile = fileChooser.getSelectedFile();
-               return new AppendStringToFileDecorator(selectedFile, string, action);
-           }
-           
-            return null;
+            
+            return new AppendStringToFileDecorator(action);
         }
-        else if (selectedType.equalsIgnoreCase("MoveFileBetweenDirs")){
-            JFileChooser fileChooser1 = new JFileChooser();
-            fileChooser1.setDialogTitle("choose file to move");
-            int res1 = fileChooser1.showOpenDialog(null);
+        else if (selectedType.equalsIgnoreCase("MoveFileToDir")){
             
-            
-            JFileChooser fileChooser2 = new JFileChooser();
-            fileChooser2.setDialogTitle("choose destination folder");
-            fileChooser2.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            int res2= fileChooser2.showOpenDialog(null);
-            
-           if(res1 == JFileChooser.APPROVE_OPTION && res2 == JFileChooser.APPROVE_OPTION){
-               File selectedFile = fileChooser1.getSelectedFile();  
-               File selectedDir = fileChooser2.getSelectedFile();
-               return new MoveFileToDirDecorator(selectedFile, selectedDir, action);
-           } 
-            return null;
+            return new MoveFileToDirDecorator(action);
         } else if (selectedType.equalsIgnoreCase("CopyFileToDir")){
-            JFileChooser fileChooser1 = new JFileChooser();
-            fileChooser1.setDialogTitle("choose file to copy");
-            int res1 = fileChooser1.showOpenDialog(null);
-            
-            
-            JFileChooser fileChooser2 = new JFileChooser();
-            fileChooser2.setDialogTitle("choose destination folder");
-            fileChooser2.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            int res2= fileChooser2.showOpenDialog(null);
-            
-           if(res1 == JFileChooser.APPROVE_OPTION && res2 == JFileChooser.APPROVE_OPTION){
-               File selectedFile = fileChooser1.getSelectedFile();  
-               File selectedDir = fileChooser2.getSelectedFile();
-               return new CopyFileToDirDecorator(selectedFile, selectedDir, action);
-           } 
-            return null; 
+           
+            return new CopyFileToDirDecorator(action); 
         }
         else if (selectedType.equalsIgnoreCase("DeleteFile")){
-           JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setDialogTitle("select file to delete");
            
-            int res = fileChooser.showOpenDialog(null);
-           if(res == JFileChooser.APPROVE_OPTION){
-               File selectedFile = fileChooser.getSelectedFile();
-               return new DeleteFileDecorator(selectedFile, action);
-           }
-           
-            return null; 
+               return new DeleteFileDecorator(action);
+
         }
         else{
             return action;
