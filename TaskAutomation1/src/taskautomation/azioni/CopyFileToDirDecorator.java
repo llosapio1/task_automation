c3557f65ab0cfe6f270c6b84fc9c2e81948390f8
Leonardo <l.losapio1@studenti.unisa.it>
2023-12-03 12:03:48 +0100

    added comments

--- TaskAutomation1/src/taskautomation/azioni/CopyFileToDirDecorator.java
+++ TaskAutomation1/src/taskautomation/azioni/CopyFileToDirDecorator.java
@@ -23,22 +23,29 @@ public class CopyFileToDirDecorator extends ActionDecorator implements Serializa
    
    File destinationDir;
    
+   //constructor used in test class
     public CopyFileToDirDecorator(File selectedFile, File destinationDir, Action basicAction) {
         super(basicAction);
         this.selectedFile = selectedFile;
-        
         this.destinationDir = destinationDir;
     }
+    
+    //constructor used in application
     public CopyFileToDirDecorator(Action basicAction){
         super(basicAction);
+        
+        //get file to copy
             JFileChooser fileChooser1 = new JFileChooser();
             fileChooser1.setDialogTitle("choose file to copy");
             int res1 = fileChooser1.showOpenDialog(null);
             
-            
+            //get destination folder to copy the file into
             JFileChooser fileChooser2 = new JFileChooser();
             fileChooser2.setDialogTitle("choose destination folder");
+            
+            //allow only the selection of directories
             fileChooser2.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            
             int res2= fileChooser2.showOpenDialog(null);
             
            if(res1 == JFileChooser.APPROVE_OPTION && res2 == JFileChooser.APPROVE_OPTION){
@@ -50,6 +57,7 @@ public class CopyFileToDirDecorator extends ActionDecorator implements Serializa
     
     @Override
     public void executeAction(){
+        //copy selected file to selected destination directory
        try {
            Files.copy(selectedFile.toPath(),
                    (new File(destinationDir.toPath() + File.separator+ selectedFile.getName())).toPath(),
