65510fbf60297eb0ddae48afb19bb71cc4c711d1
Alejandro <Alejandro@DESKTOP-IJ0RFSQ.mshome.net>
2023-12-04 16:36:15 +0100

    Bug Check.Rule

--- TaskAutomation1/src/taskautomation/rule/Rule.java
+++ TaskAutomation1/src/taskautomation/rule/Rule.java
@@ -88,6 +88,7 @@ public class Rule implements Serializable{
     
     public void checkRule() {
         if (this.active && !alreadyFired && this.fireOnlyOnce) {
+            System.out.println("1");
             if (this.trigger.verifyTrigger()) {
                 this.action.executeAction();
                 this.lastFired = LocalTime.now();
