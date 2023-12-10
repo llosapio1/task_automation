9ff0ba2cd58735806f613562c9d9c32ed64348c1
Leonardo <l.losapio1@studenti.unisa.it>
2023-12-10 11:13:53 +0100

    added comments

--- TaskAutomation1/tests/taskautomation/trigger/ReturnCodeIsEqualDecoratorTest.java
+++ TaskAutomation1/tests/taskautomation/trigger/ReturnCodeIsEqualDecoratorTest.java
@@ -30,17 +30,20 @@ public class ReturnCodeIsEqualDecoratorTest {
     @Test
     public void testVerifyTrigger() {
         System.out.println("verifyTrigger");
+        
+        //get test application from this project's folder
         File program = new File("greetings.exe");
-        String arguments = "";
-        int expValue = 0;
+        String arguments = ""; //arguments to execute application with
+        int expValue = 0; //value to check return code against
         
         Trigger decoratedTrigger = new BasicTrigger();
         ReturnCodeIsEqualDecorator instance1= new ReturnCodeIsEqualDecorator(program, arguments, expValue, decoratedTrigger);
         
         boolean expResult = true;
-        boolean result = instance1.verifyTrigger();
+        boolean result = instance1.verifyTrigger();  //check if test application's return code is equal to given value
         assertEquals(expResult, result);
         
+        //same as above, test with different arguments
         arguments = "testing";
         expValue = 2;
         
@@ -50,7 +53,7 @@ public class ReturnCodeIsEqualDecoratorTest {
         result = instance2.verifyTrigger();
         assertEquals(expResult, result);
         
-        
+        //same as above, test with different value to check against
         expValue = 3;
         
         ReturnCodeIsEqualDecorator instance3= new ReturnCodeIsEqualDecorator(program, arguments, expValue, decoratedTrigger);
@@ -67,6 +70,7 @@ public class ReturnCodeIsEqualDecoratorTest {
     public void testToString() {
        System.out.println("toString");
        
+       //use external program name, arguments and value for decorator's constructor
         File program = new File("greetings.exe");
         String arguments = "";
         int expValue = 0;
@@ -78,7 +82,7 @@ public class ReturnCodeIsEqualDecoratorTest {
         String result = instance1.toString();
         assertEquals(expResult, result);
         
-        
+        //same as above
        program = new File("testing.exe");
        arguments = "test";
        expValue = 1;
