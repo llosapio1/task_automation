3df3d99073c7b1ab7cd835b371d736b8a297dc94
Leonardo <l.losapio1@studenti.unisa.it>
2023-12-10 09:59:25 +0100

    added comments

--- TaskAutomation1/src/taskautomation/azioni/AddCounterToCounterDecorator.java
+++ TaskAutomation1/src/taskautomation/azioni/AddCounterToCounterDecorator.java
@@ -17,20 +17,28 @@ import taskautomation.counter.CounterList;
 public class AddCounterToCounterDecorator extends ActionDecorator implements Serializable{
     String counterName1;
     String counterName2;
+    
+    //constructor used in test class
     public AddCounterToCounterDecorator(String counterName1, String counterName2, Action basicAction){
         super(basicAction);
         this.counterName1 = counterName1;
         this.counterName2 = counterName2;
         
     }
+    
+    //constructor used in application
     public AddCounterToCounterDecorator(Action basicAction){
         super(basicAction);
+        
+        //get name of first counter from user input
          TextInputDialog dialog = new TextInputDialog("Type the name of the first counter.");
         dialog.setHeaderText(null);
         dialog.setTitle("add counter to counter");
         dialog.setContentText("Type the counter to add");
         this.counterName1 = dialog.showAndWait().orElse("");
         
+        
+        //get name of second counter from user input
         TextInputDialog dialog2 = new TextInputDialog("Type the name of the second counter");
         dialog.setHeaderText(null);
         dialog.setTitle("add counter to counter");
@@ -43,25 +51,32 @@ public class AddCounterToCounterDecorator extends ActionDecorator implements Ser
      public void executeAction(){
          Counter counter1 = null;
          Counter counter2 = null;
+         
+         //use these to check if counters have been found in counter list
         boolean found1 = false;
         boolean found2 = false;
         
          ArrayList<Counter> list = CounterList.getCounterList().get();
          for (Counter c : list){
             
+             //found counter1
              if (c.getName() == this.counterName1){
                  counter1 = c;
                  found1= true;
              }  
+             
+             //found counter2
              if(c.getName() == this.counterName2){
                  counter2 = c;
                  found2= true;
              }
+             
+             //exit when counter1 and counter2 have both been found
              if(found1 && found2){
                  break;
              }
          }
-
+         //update counter2's value with sum of counter2's old value + counter1's value
          CounterList.getCounterList().updateCounter(counter2, counter2.getValue()+counter1.getValue());
          
      }
