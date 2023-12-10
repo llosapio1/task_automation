c3cf08c3c7ed5a75159eeb1ac89b06d694c17638
Leonardo <l.losapio1@studenti.unisa.it>
2023-12-10 11:03:50 +0100

    added comments

--- TaskAutomation1/tests/taskautomation/trigger/CounterIsLessThanCounterDecoratorTest.java
+++ TaskAutomation1/tests/taskautomation/trigger/CounterIsLessThanCounterDecoratorTest.java
@@ -32,6 +32,7 @@ public class CounterIsLessThanCounterDecoratorTest {
     public void testVerifyTrigger() {
         System.out.println("verifyTrigger");
        
+        //create two counters with names and values, then add them to counter list
         String counterName1 = "testing";
         int counterValue1 = 5;
         String counterName2 = "testing2";
@@ -45,11 +46,12 @@ public class CounterIsLessThanCounterDecoratorTest {
         
         BasicTrigger trigger = new BasicTrigger();
         CounterIsLessThanCounterDecorator instance = new CounterIsLessThanCounterDecorator(counterName1, counterName2, trigger);
-        boolean result = instance.verifyTrigger();
+        boolean result = instance.verifyTrigger();  //check if one counter's value is less than the other's
         boolean exp= false;
         
         assertEquals(exp, result);
         
+        //get test counters from counter list, then delete them
         int index1 = CounterList.getCounterList().get().indexOf(counter1); 
         Counter c1 = CounterList.getCounterList().get().get(index1);
         CounterList.getCounterList().removeCounter(c1);
@@ -58,7 +60,7 @@ public class CounterIsLessThanCounterDecoratorTest {
         CounterList.getCounterList().removeCounter(c2);
         
         
-        
+        //same as above
         counterName1 = "abc";
         counterValue1 = 5;
         counterName2 = "cdf";
@@ -90,6 +92,7 @@ public class CounterIsLessThanCounterDecoratorTest {
     public void testToString() {
         System.out.println("toString");
  
+        //use two counter names for decorator's constructor
         String counterName1 = "test1counter";
         String counterName2 = "test2counter";
         BasicTrigger trigger = new BasicTrigger();
@@ -99,7 +102,7 @@ public class CounterIsLessThanCounterDecoratorTest {
         String result = instance.toString();
         assertEquals(expResult, result);
         
-        
+        //same as above
         counterName1 = "hello";
         counterName2 = "world";
         
