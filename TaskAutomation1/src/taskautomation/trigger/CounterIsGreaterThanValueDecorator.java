849f2ffb16022a924053fb1c241c869763d47625
Leonardo <l.losapio1@studenti.unisa.it>
2023-12-10 10:13:23 +0100

    added comments

--- TaskAutomation1/src/taskautomation/trigger/CounterIsGreaterThanValueDecorator.java
+++ TaskAutomation1/src/taskautomation/trigger/CounterIsGreaterThanValueDecorator.java
@@ -18,19 +18,26 @@ public class CounterIsGreaterThanValueDecorator extends TriggerDecorator impleme
     String counterName;
     int value;
     
+    //constructor used in test class
     public CounterIsGreaterThanValueDecorator(String counterName, int value, Trigger decoratedTrigger){
         super(decoratedTrigger);
         this.counterName = counterName;
         this.value = value;
     }
+    
+    //constructor used in application
     public CounterIsGreaterThanValueDecorator(Trigger decoratedTrigger){
         super(decoratedTrigger);
+        
+        //get name of the counter from user input
         TextInputDialog dialog = new TextInputDialog("type the name of the counter");
         dialog.setHeaderText(null);
         dialog.setTitle("check if counter is greater than given value");
         dialog.setContentText("name of the counter");
         this.counterName = dialog.showAndWait().orElse("");
         
+        
+        //get value to check against from user input
         TextInputDialog dialog2 = new TextInputDialog("type the value to check against");
         dialog2.setHeaderText(null);
         dialog2.setTitle("check if counter is greater than given value");
@@ -42,13 +49,19 @@ public class CounterIsGreaterThanValueDecorator extends TriggerDecorator impleme
     public boolean verifyTrigger(){
         Counter counter = null;
          ArrayList<Counter> list = CounterList.getCounterList().get();
+         
+         //look for counter in counter list
          for (Counter c : list){
              if (c.getName() == this.counterName){
                  counter = c;
                  break;
              }
          }
+         
+         if(counter!=null){
+         //check if counter's value is greater than number chosen by user
          return (counter.getValue() > this.value);
+         }else return false;
       
     }
     
