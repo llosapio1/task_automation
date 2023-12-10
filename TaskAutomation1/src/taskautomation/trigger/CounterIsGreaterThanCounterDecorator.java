0d8be0ae02e71156fa78a7e6d6d363c86976b1ca
Leonardo <l.losapio1@studenti.unisa.it>
2023-12-10 10:11:43 +0100

    added comments

--- TaskAutomation1/src/taskautomation/trigger/CounterIsGreaterThanCounterDecorator.java
+++ TaskAutomation1/src/taskautomation/trigger/CounterIsGreaterThanCounterDecorator.java
@@ -18,20 +18,25 @@ public class CounterIsGreaterThanCounterDecorator extends TriggerDecorator imple
     String counterName1;
     String counterName2;
     
+    //constructor used in test class
     public CounterIsGreaterThanCounterDecorator(String counterName1, String counterName2, Trigger decoratedTrigger){
         super(decoratedTrigger);
         this.counterName1 = counterName1;
         this.counterName2 = counterName2;
     }
     
+    //constructor used in application
     public CounterIsGreaterThanCounterDecorator(Trigger decoratedTrigger){
         super(decoratedTrigger);
+        
+        //get the name of the first counter from user input
         TextInputDialog dialog = new TextInputDialog("type the name of the first counter");
         dialog.setHeaderText(null);
         dialog.setTitle("check if counter is greater than counter");
         dialog.setContentText("name of the first counter");
         this.counterName1 = dialog.showAndWait().orElse("");
         
+        //get the name of the second counter from user input
         TextInputDialog dialog2 = new TextInputDialog("type the name of the second counter");
         dialog2.setHeaderText(null);
         dialog2.setTitle("check if counter is greater than counter");
@@ -44,22 +49,33 @@ public class CounterIsGreaterThanCounterDecorator extends TriggerDecorator imple
         Counter counter1 = null;
         Counter counter2 = null;
          ArrayList<Counter> list = CounterList.getCounterList().get();
+         
+         //use these to check whether both counters have been found
          boolean found1 = false;
          boolean found2 = false;
+         
          for (Counter c : list){
+             
+             //counter1 has been found
              if (c.getName() == this.counterName1){
                  counter1 = c;
                  found1 = true;
              }
+             
+             //counter2 has been found
              if(c.getName() == this.counterName2){
                  counter2 = c;
                  found2 = true;
              }
+             
+             //if both counters have been found, exit for
              if(found1 && found2){
                  break;
              }
          }
+         if (counter1!=null && counter2!=null){
          return (counter1.getValue() > counter2.getValue());
+         }else return false;
       
     }
     
